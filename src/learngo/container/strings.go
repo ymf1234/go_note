0afac0a4dd72f3abaffae7797c3ee7a79a7be1bd
yumengfei <mengfei_yu@loongjoy.com>
2021-11-17 16:17:52 +0800

    字符串

--- src/learngo/container/strings.go
+++ src/learngo/container/strings.go
@@ -6,35 +6,39 @@ import (
 )
 
 func main() {
-	s := "GoLang语言"
+	s := "Go语言"
 
-	for _, b := range []byte(s) {
-		fmt.Printf("%X ", b)
+	fmt.Println(s)
 
-		fmt.Println(s)
-
-		fmt.Println()
-
-		for i, ch := range s {
-			fmt.Printf("( %d %X )", i, ch)
-		}
-
-		fmt.Println()
-
-		fmt.Println(" Rune count:", utf8.RuneCountInString(s))
-
-		bytes := []byte(s)
-
-		for len(bytes) > 0 {
-			ch, size := utf8.DecodeRune(bytes)
-			bytes = bytes[size:] // 切片
-			fmt.Printf("%c ", ch)
-		}
-		fmt.Println()
-
-		for i, ch := range []rune(s) {
-			fmt.Printf("( %d %c ) ", i, ch)
-		}
-		fmt.Println()
+	for  _,i := range s {
+		fmt.Printf("%X ", i)
 	}
+
+	fmt.Println()
+
+	for i, ch := range s {
+		fmt.Printf("( %d %X )", i, ch)
+	}
+
+	fmt.Println()
+
+	fmt.Println("Rune count ", utf8.RuneCountInString(s))
+
+	bytes := []byte(s)
+
+	for len(bytes) > 0 {
+		ch, size := utf8.DecodeRune(bytes)
+		bytes = bytes[size:] // 切片
+		fmt.Printf("%c ", ch)
+	}
+	fmt.Println()
+
+	for i , ch := range []rune(s) {
+		fmt.Printf("( %d %c ) ", i, ch)
+	}
+	fmt.Println()
+
+
+
+
 }
