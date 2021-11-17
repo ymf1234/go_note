28c6d522f0c8be52a761709f20c813e8c0fa6b26
yumengfei <mengfei_yu@loongjoy.com>
2021-11-17 19:34:26 +0800

    包

---
+++ src/learngo/tree/enrey/entry.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"learngo/tree"
+)
+
+/**
+  封装：
+	  名字一般使用大驼峰 例如：CamelCase
+	  首字母大写：public
+	  首字母小写：private
+  包：
+     每个目录一个包
+     main包包含可执行入口文件
+     为结构定义的方法必须放在同一个包内
+     可以是不同的文件
+
+	 如何扩展系统类型或者别人的类型
+		定义别名
+		使用组合
+ */
+
+type myTreeNode struct {
+	node *tree.Node
+}
+
+func (myNode *myTreeNode) postOrder() {
+	if myNode == nil || myNode.node == nil {
+		return
+	}
+
+	left := myTreeNode{myNode.node.Left}
+	left.postOrder()
+	right := myTreeNode{myNode.node.Right}
+	right.postOrder()
+	myNode.node.Print()
+}
+
+func main() {
+	var root tree.Node
+
+	root = tree.Node{Value: 3}
+	root.Left = &tree.Node{}
+	root.Right = &tree.Node{5, nil, nil}
+	root.Right.Left = new(tree.Node)
+	root.Left.Right = tree.CreateNode(2)
+
+	fmt.Println(root)
+	root.Print()
+	root.Right.Left.SetValue(4)
+	root.Right.Left.Print()
+	fmt.Println("---------------------")
+
+	root.Traverse()
+	fmt.Println("---------------------")
+	node := myTreeNode{&root}
+	node.postOrder()
+
+	//nodes := []treeNode{
+	//	{value: 5},
+	//	{},
+	//	{6, nil, &root},
+	//}
+	//
+	//fmt.Println(nodes)
+
+
+}
--- src/learngo/tree/node.go
+++ src/learngo/tree/node.go
@@ -1,48 +1,23 @@
-package main
+package tree
 
 import "fmt"
 
 // 结构体
-type treeNode struct {
-	value int
-	left, right *treeNode
+type Node struct {
+	Value       int
+	Left, Right *Node
 }
 
-func (node treeNode) print()  {
-	fmt.Println(node.value)
+func (node Node) Print()  {
+	fmt.Println(node.Value)
 }
 
-func (node *treeNode) setValue(value int) {
-	node.value = value
+func (node *Node) SetValue(value int) {
+	node.Value = value
 }
 
-func createNode(value int) *treeNode {
-	return &treeNode{value: value}
+func CreateNode(value int) *Node {
+	return &Node{Value: value}
 }
 
 
-func main() {
-	var root treeNode
-
-	root = treeNode{value: 3}
-	root.left = &treeNode{}
-	root.right = &treeNode{5, nil, nil}
-	root.right.left = new(treeNode)
-	root.left.right = createNode(2)
-
-	fmt.Println(root)
-	root.print()
-	root.right.left.setValue(4)
-	root.right.left.print()
-	fmt.Println("---------------------")
-
-	nodes := []treeNode{
-		{value: 5},
-		{},
-		{6, nil, &root},
-	}
-
-	fmt.Println(nodes)
-
-
-}
