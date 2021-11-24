9b99c8981c7dca8d695f585ddc28a34ff6458c38
yumengfei <mengfei_yu@loongjoy.com>
2021-11-24 12:24:21 +0800

    修改

--- learngos/errhandling/filelistringserver/filelisting/handle.go
+++ learngos/errhandling/filelistringserver/filelisting/handle.go
@@ -6,19 +6,20 @@ import (
 	"os"
 )
 
-func HandleFileList(writer http.ResponseWriter, request *http.Request) {
+func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
 	path := request.URL.Path[len("/list/"):]
 	file, err := os.Open(path)
 	if err != nil {
-		http.Error(writer, err.Error(), http.StatusInternalServerError)
-		return
+		//http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return err
 	}
 	defer file.Close()
 
 	all, err := ioutil.ReadAll(file)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	writer.Write(all)
+	return nil
 }
--- learngos/errhandling/filelistringserver/web.go
+++ learngos/errhandling/filelistringserver/web.go
@@ -1,17 +1,28 @@
 package main
 
 import (
-	"io/ioutil"
 	"learngos/errhandling/filelistringserver/filelisting"
 	"net/http"
 	"os"
 )
 
+type addHandler func(writer http.ResponseWriter, request *http.Response) error
 
+func errWrapper(handler addHandler) func(http.ResponseWriter, *http.Response) {
+	return func(writer http.ResponseWriter, response *http.Response) {
+		err := handler(writer, response)
+		if err != nil {
+			switch {
+			case os.IsNotExist(err):
+				http.Error(writer, http.StatusText(http.StatusNotFound))
+			}
+		}
+	}
+}
 
 func main() {
 	http.HandleFunc("/list/",
-		filelisting.HandleFileList())
+		errWrapper(filelisting.HandleFileList))
 
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
